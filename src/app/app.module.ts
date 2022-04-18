4bb0ea5c6cf461f36c86c3faf6f2fce6fc8a0c4a
David Musembi <davidmusembi02@gmail.com>
2022-04-18 22:29:27 +0300

    Create a directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer/footer.component';
-import { MainpageComponent } from './homepage/mainpage/mainpage.component';
-import { NavBarComponent } from './nav-bar/nav-bar/nav-bar.component';
-import { NotFoundComponent } from './not-found/not-found/not-found.component';
-import { RepositorySearchComponent } from './repository-search/repository-search/repository-search.component';
-import { UserSearchComponent } from './user-search/user-search/user-search.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RepositorySearchComponent } from './repository-search/repository-search.component';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { FormsModule } from '@angular/forms';
+import { StarRepositoryDirective } from './star-repository.directive';
 
 @NgModule({
   declarations: [
@@ -18,11 +21,14 @@ import { UserSearchComponent } from './user-search/user-search/user-search.compo
     NavBarComponent,
     NotFoundComponent,
     RepositorySearchComponent,
-    UserSearchComponent
+    UserSearchComponent,
+    StarRepositoryDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
