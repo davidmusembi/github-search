bff3d9e40e28df522c92d2c50f98a759be828e86
David Musembi <davidmusembi02@gmail.com>
2022-04-18 22:03:01 +0300

    Add mainpage for github

---
+++ src/app/mainpage/mainpage.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { RepositoryUserService } from '../repository-user.service';
+import { User } from '../user';
+
+
+@Component({
+  selector: 'app-mainpage',
+  templateUrl: './mainpage.component.html',
+  styleUrls: ['./mainpage.component.css']
+})
+export class MainpageComponent implements OnInit {
+
+  user!: User;
+  repositories: any;
+  constructor(private repositoryUserService: RepositoryUserService) { }
+
+  ngOnInit(): void {
+    this.getUserDetails('davidmusembi');
+    this.getUserRepositories('davidmusembi');
+  }
+
+  //user details
+  getUserDetails(githubUsername: string) {
+    this.repositoryUserService.getUserResponse(githubUsername).then(
+      (response: any) => {
+        this.user = this.repositoryUserService.getUserDetails;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    ); 
+  }
+
+
+  //user repositories
+  getUserRepositories(githubUsername: string) {
+    this.repositoryUserService.getRepositoryResponse(githubUsername).then(
+      (response: any) => {
+        this.repositories = this.repositoryUserService.getRepositoryDetails;
+        console.log(this.repositories);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+}
