bc05e3d50021f7dc3b97d3c99b87ad14521ed392
David Musembi <davidmusembi02@gmail.com>
2022-04-18 16:52:54 +0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { MainpageComponent } from './homepage/mainpage/mainpage.component';
+import { NavBarComponent } from './nav-bar/nav-bar/nav-bar.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
+import { RepositorySearchComponent } from './repository-search/repository-search/repository-search.component';
+import { UserSearchComponent } from './user-search/user-search/user-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainpageComponent,
+    NavBarComponent,
+    NotFoundComponent,
+    RepositorySearchComponent,
+    UserSearchComponent
   ],
   imports: [
     BrowserModule,
